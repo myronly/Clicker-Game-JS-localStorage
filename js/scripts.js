1848321a1f82bf5846e43421b6cd1a3b854fe1a0
Miron <kolomiietsnazar@gmail.com>
2022-08-07 04:15:14 +0300

    update 1.7

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,44 @@
 // Custom Scripts
 document.addEventListener("DOMContentLoaded", () => {
+  let count, perSecond;
+
+  const clickerShopPerClick = document.querySelector(".clicker__shop-per-click");
+  const clickerShopPerSecond = document.querySelector(".clicker__shop-per-second");
+
+  const PRICE_INCREASE = 1.25;
+  const PER_SECOND_PRICE_ARR = [20, 100, 500, 2500, 12500];
+  const PER_SECOND_COUNT_ARR = [0.1, 1, 5, 25, 125];
+  const PER_CLICK_PRICE_ARR = [50, 200, 1000, 5000, 20000];
+  const PER_CLICK_COUNT_ARR = [0.1, 1, 5, 10, 25];
+
+  function CreateUp(selector, length) {
+    for (let i = 0; i < length.length; i++) {
+      const element = document.createElement("div");
+      element.classList.add("clicker__shop-up");
+      if (selector.classList.contains("clicker__shop-per-click")) {
+        perSecond = "clicker__shop-lvl-per-click";
+        amountAndCount = "click";
+        name = "click";
+      } else {
+        perSecond = "clicker__shop-lvl-per-second";
+        amountAndCount = "second";
+        name = "sec";
+      }
+      element.innerHTML = `
+        <div class="clicker__shop-price not-enough">
+          <p>
+            <span class="clicker__shop-price-amount per__${amountAndCount}-amount per__click-amount">0</span>
+            <span class="clicker__shop-price-count per__${amountAndCount}-count per__click-count">0</span>
+          </p>
+          <img src="img/cookie.png" alt="cookie" />
+        </div>
+        <div class="clicker__shop-lvl ${perSecond} not-enough">+${length[i]}/${name}</div>`;
+      selector.append(element);
+    }
+  }
+  CreateUp(clickerShopPerClick, PER_CLICK_COUNT_ARR);
+  CreateUp(clickerShopPerSecond, PER_SECOND_COUNT_ARR);
+
   const clicker = document.querySelector(".clicker__btn");
   const countClick = document.querySelector(".clicker__count");
   const clickerUP = document.querySelectorAll(".clicker__shop-lvl");
@@ -15,14 +54,6 @@ document.addEventListener("DOMContentLoaded", () => {
   const clickerPerSecond = document.querySelector(".clicker__seconds");
   const clickerPerClick = document.querySelector(".clicker__clicks");
   const reset = document.querySelector(".reset");
-
-  let count, perSecond;
-
-  const PRICE_INCREASE = 1.15;
-  const PER_SECOND_PRICE_ARR = [20, 100, 500, 2500, 12500];
-  const PER_SECOND_COUNT_ARR = [0.1, 1, 5, 25, 125];
-  const PER_CLICK_PRICE_ARR = [50, 200, 1000, 5000, 20000];
-  const PER_CLICK_COUNT_ARR = [0.1, 1, 5, 25, 125];
 
   if (
     localStorage.getItem("count") !== null &&
@@ -62,104 +93,126 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 
   for (let i = 0; i < clickerUP.length; i++) {
-    setInterval(
-      () =>
-        +clickerUP[i].dataset.price > count
-          ? clickerUP[i].classList.add("not-enough") ||
-            clickerUpAmount[i].parentNode.parentNode.classList.add("not-enough")
-          : clickerUP[i].classList.remove("not-enough") ||
-            clickerUpAmount[i].parentNode.parentNode.classList.remove("not-enough"),
-      100
-    );
+    setInterval(() => {
+      if (clickerUP[i].classList.contains("click-up")) {
+        setTimeout(() => {
+          clickerUP[i].classList.remove("click-up");
+          clickerUpAmount[i].parentNode.parentNode.classList.remove("click-up");
+        }, 200);
+      }
+      if (+clickerUP[i].dataset.price > count) {
+        clickerUP[i].classList.add("not-enough");
+        clickerUpAmount[i].parentNode.parentNode.classList.add("not-enough");
+      } else {
+        clickerUP[i].classList.remove("not-enough");
+        clickerUpAmount[i].parentNode.parentNode.classList.remove("not-enough");
+      }
+    }, 50);
+    setInterval(() => {
+      if (+clickerUP[i].dataset.price < count) {
+        clickerUP[i].classList.add("click-up");
+        clickerUpAmount[i].parentNode.parentNode.classList.add("click-up");
+        setTimeout(() => {
+          clickerUP[i].classList.remove("click-up");
+          clickerUpAmount[i].parentNode.parentNode.classList.remove("click-up");
+        }, 200);
+      }
+    }, 200);
   }
 
-  for (let i = 0; i < clickerPerSecondUP.length; i++) {
-    clickerPerSecondUP[i].setAttribute("data-price", PER_SECOND_PRICE_ARR[i]);
-    clickerPerSecondUP[i].setAttribute("data-count", PER_SECOND_COUNT_ARR[i]);
-    clickerPerClickUP[i].setAttribute("data-price", PER_CLICK_PRICE_ARR[i]);
-    clickerPerClickUP[i].setAttribute("data-count", PER_CLICK_COUNT_ARR[i]);
-
-    clickerPerSecondUP[i].addEventListener("click", () => {
-      if (
-        +clickerPerSecondUP[i].dataset.price <= count &&
-        !clickerPerSecondUP[i].classList.contains("not-enough")
-      ) {
-        count -= +clickerPerSecondUP[i].dataset.price;
-        perSecond = +perSecond + +clickerPerSecondUP[i].dataset.count;
-        localStorage.setItem("perSecond", perSecond);
-        let STEP = 1;
-        const interval = setInterval(() => {
-          STEP = STEP * 1.5;
-          clickerPerSecond.textContent = Math.round(+clickerPerSecond.textContent + STEP).toFixed(
-            1
-          );
-          if (+clickerPerSecond.textContent >= +localStorage.getItem("perSecond")) {
-            clearInterval(interval);
-            clickerPerSecond.textContent = Number(localStorage.getItem("perSecond")).toFixed(1);
+  function perUP(clickerUp, clickerUpAmount, clickerUpCount, getUP) {
+    if (clickerUp[0].parentNode.parentNode.classList.contains("clicker__shop-per-click")) {
+      for (let i = 0; i < clickerUp.length; i++) {
+        clickerUp[i].setAttribute("data-price", PER_CLICK_PRICE_ARR[i]);
+        clickerUp[i].setAttribute("data-count", PER_CLICK_COUNT_ARR[i]);
+        clickerUp[i].addEventListener("click", () => {
+          if (
+            +clickerUp[i].dataset.price <= count &&
+            !clickerUp[i].classList.contains("not-enough")
+          ) {
+            count -= +clickerUp[i].dataset.price;
+            perClick += +clickerUp[i].dataset.count;
+            localStorage.setItem("count", count);
+            localStorage.setItem("perClick", perClick);
+            clickerPerClick.textContent = perClick.toFixed(1);
+            clickerUp[i].classList.add("click-up");
+            clickerUpAmount[i].parentNode.parentNode.classList.add("click-up");
+            if (clickerUp[i].classList.contains("click-up")) {
+              setTimeout(() => {
+                clickerUp[i].classList.remove("click-up");
+                clickerUpAmount[i].parentNode.parentNode.classList.remove("click-up");
+              }, 200);
+            }
           }
-        }, 4);
-        localStorage.setItem("count", count);
-        clickerPerSecondUP[i].classList.add("click-up");
-        clickerPerSecondUpAmount[i].parentNode.parentNode.classList.add("click-up");
-        if (clickerPerSecondUP[i].classList.contains("click-up")) {
-          setTimeout(() => {
-            clickerPerSecondUP[i].classList.remove("click-up");
-            clickerPerSecondUpAmount[i].parentNode.parentNode.classList.remove("click-up");
-          }, 200);
-        }
+        });
       }
-    });
-
-    clickerPerClickUP[i].addEventListener("click", () => {
-      if (
-        +clickerPerClickUP[i].dataset.price <= count &&
-        !clickerPerClickUP[i].classList.contains("not-enough")
-      ) {
-        count -= +clickerPerClickUP[i].dataset.price;
-        perClick += +clickerPerClickUP[i].dataset.count;
-        localStorage.setItem("count", count);
-        localStorage.setItem("perClick", perClick);
-        clickerPerClick.textContent = perClick.toFixed(1);
-        clickerPerClickUP[i].classList.add("click-up");
-        clickerPerClickUPAmount[i].parentNode.parentNode.classList.add("click-up");
-        if (clickerPerClickUP[i].classList.contains("click-up")) {
-          setTimeout(() => {
-            clickerPerClickUP[i].classList.remove("click-up");
-            clickerPerClickUPAmount[i].parentNode.parentNode.classList.remove("click-up");
-          }, 200);
-        }
+    } else {
+      for (let i = 0; i < clickerUp.length; i++) {
+        clickerUp[i].setAttribute("data-price", PER_SECOND_PRICE_ARR[i]);
+        clickerUp[i].setAttribute("data-count", PER_SECOND_COUNT_ARR[i]);
+        clickerUp[i].addEventListener("click", () => {
+          if (
+            +clickerUp[i].dataset.price <= count &&
+            !clickerUp[i].classList.contains("not-enough")
+          ) {
+            count -= +clickerUp[i].dataset.price;
+            perSecond = +perSecond + +clickerUp[i].dataset.count;
+            localStorage.setItem("perSecond", perSecond);
+            let STEP = 1;
+            const interval = setInterval(() => {
+              STEP = STEP * 1.5;
+              clickerPerSecond.textContent = Math.round(
+                +clickerPerSecond.textContent + STEP
+              ).toFixed(1);
+              if (+clickerPerSecond.textContent >= +localStorage.getItem("perSecond")) {
+                clearInterval(interval);
+                clickerPerSecond.textContent = Number(localStorage.getItem("perSecond")).toFixed(1);
+              }
+            }, 4);
+            localStorage.setItem("count", count);
+            clickerUp[i].classList.add("click-up");
+            clickerUpAmount[i].parentNode.parentNode.classList.add("click-up");
+            if (clickerUp[i].classList.contains("click-up")) {
+              setTimeout(() => {
+                clickerUp[i].classList.remove("click-up");
+                clickerUpAmount[i].parentNode.parentNode.classList.remove("click-up");
+              }, 200);
+            }
+          }
+        });
       }
-    });
-  }
-
-  function perUP(lvl_UP, clickerUpAmount, clickerUpCount, getUP) {
+    }
     let amountArr = [];
-    for (let i = 0; i < lvl_UP.length; i++) {
+    for (let i = 0; i < clickerUp.length; i++) {
       let amount = 0;
       let amountArrGet = localStorage.getItem(getUP).split(",");
       amount = amountArrGet[i];
       clickerUpAmount[i].textContent = amount + "x";
       amountArr.push(amount);
       if (+amount !== 0) {
-        lvl_UP[i].dataset.price = Math.round(lvl_UP[i].dataset.price * PRICE_INCREASE ** amount);
+        clickerUp[i].dataset.price = (
+          clickerUp[i].dataset.price *
+          PRICE_INCREASE ** amount
+        ).toFixed(1);
       }
-      clickerUpCount[i].textContent = lvl_UP[i].dataset.price;
-      lvl_UP[i].addEventListener("click", () => {
-        if (!lvl_UP[i].classList.contains("not-enough")) {
+      clickerUpCount[i].textContent = (+clickerUp[i].dataset.price).toFixed(1);
+      clickerUp[i].addEventListener("click", () => {
+        if (!clickerUp[i].classList.contains("not-enough")) {
           clickerUpAmount[i].textContent = ++amount + "x";
           amountArr[i] = amount;
           localStorage.setItem(getUP, amountArr);
           let STEP = 1;
-          const priceIncreasePrimary = +lvl_UP[i].dataset.price;
+          const priceIncreasePrimary = +clickerUp[i].dataset.price;
           const interval = setInterval(() => {
             STEP = STEP * 1.5;
-            clickerUpCount[i].textContent = Math.round(+lvl_UP[i].dataset.price + STEP);
+            clickerUpCount[i].textContent = (+clickerUp[i].dataset.price + STEP).toFixed(1);
             if (
-              +clickerUpCount[i].textContent >= Math.round(lvl_UP[i].dataset.price * PRICE_INCREASE)
+              +clickerUpCount[i].textContent >=
+              Math.round(clickerUp[i].dataset.price * PRICE_INCREASE)
             ) {
               clearInterval(interval);
-              lvl_UP[i].dataset.price = Math.round(priceIncreasePrimary * PRICE_INCREASE);
-              clickerUpCount[i].textContent = lvl_UP[i].dataset.price;
+              clickerUp[i].dataset.price = priceIncreasePrimary * PRICE_INCREASE;
+              clickerUpCount[i].textContent = (+clickerUp[i].dataset.price).toFixed(1);
             }
           }, 4);
         }
@@ -182,18 +235,23 @@ document.addEventListener("DOMContentLoaded", () => {
     count = +count + +perSecond / 100;
     localStorage.setItem("count", count);
     countClick.textContent = count.toFixed(1);
-    for (let i = 0; i < clickerPerClickUP.length; i++) {
-      clickerPerClickUP[i].dataset.price = clickerPerClickUPCount[i].textContent;
-      clickerPerClickUP[i].dataset.count = PER_CLICK_COUNT_ARR[i];
-      clickerPerSecondUP[i].dataset.price = clickerPerSecondUPCount[i].textContent;
-      clickerPerSecondUP[i].dataset.count = PER_SECOND_COUNT_ARR[i];
+    if (clickerPerClickUP[0].parentNode.parentNode.classList.contains("clicker__shop-per-click")) {
+      for (let i = 0; i < clickerPerClickUP.length; i++) {
+        clickerPerClickUP[i].dataset.price = clickerPerClickUPCount[i].textContent;
+        clickerPerClickUP[i].dataset.count = PER_CLICK_COUNT_ARR[i];
+      }
+    } else {
+      for (let i = 0; i < clickerPerSecondUP.length; i++) {
+        clickerPerSecondUP[i].dataset.price = clickerPerSecondUPCount[i].textContent;
+        clickerPerSecondUP[i].dataset.count = PER_SECOND_COUNT_ARR[i];
+      }
     }
   }
 
   const interval = setInterval(perInterval, 10);
   reset.addEventListener("click", () => {
     clearInterval(interval);
-    count = 0;
+    count = 107;
     perClick = 1;
     perSecond = 0;
     localStorage.setItem("count", count);
